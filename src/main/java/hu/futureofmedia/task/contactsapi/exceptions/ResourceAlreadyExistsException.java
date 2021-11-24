a6549d1fbfc132b0e1cbe6836c28aa09f78537dd
Your Name <szucsi.daniel123@gmail.com>
2021-11-24 14:56:35 +0100

    services + DTOs

---
+++ src/main/java/hu/futureofmedia/task/contactsapi/exceptions/ResourceAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package hu.futureofmedia.task.contactsapi.exceptions;
+
+public class ResourceAlreadyExistsException extends RuntimeException {
+
+  public ResourceAlreadyExistsException(String msg) {
+    super(msg);
+  }
+
+}
---
+++ src/main/java/hu/futureofmedia/task/contactsapi/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package hu.futureofmedia.task.contactsapi.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+  public ResourceNotFoundException(String msg) {
+    super(msg);
+  }
+
+}
